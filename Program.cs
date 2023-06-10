1dd36fa2555ccb4af98aa5ae85bf750058acca44
scottmckendry <me@scottmckendry.tech>
2023-06-10 12:08:23 +1200

    fixed package refs + some other small issue Caused by pulling out of another project

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ string[] scopes = { "https://graph.microsoft.com/.default" };
 string tenantId = "common";
 string clientId = "3d04380f-2420-4eb9-ba3b-28f07e1ef5f4";
 
-// Create new Token using KPT Identity
+// Create new Token
 Token token = new(tenantId, clientId, scopes);
 string? accessToken = token.GetToken().Result;
 
